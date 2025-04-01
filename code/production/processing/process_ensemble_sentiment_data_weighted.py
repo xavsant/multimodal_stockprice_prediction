00704a86f627610e6870bb2f0540f64c1d1ce900
frostedtrees <t.r.t.trina@gmail.com>
2025-04-01 15:40:06 +0800

    add weighted sentiment files, window size 6, sentiment effect 0.01

---
+++ code/production/processing/process_ensemble_sentiment_data_weighted.py
@@ -0,0 +1,76 @@
+# Note: requires stock data - need to reference dates for sentiment aggregation 
+# Run after process_stock_data.py
+
+# Imports
+from pandas import read_csv, to_datetime, Timedelta
+from numpy import where, ceil, floor
+from dotenv import load_dotenv
+from os import getenv
+from datetime import timedelta
+
+# Function to aggregate sentiment using a voting system
+def aggregate_sentiment(row):
+    sentiment_map = {'Positive': 1, 'Neutral': 0, 'Negative': -1}
+    
+    sentiments = [row['vader_label'], row['drob_label'], row['deb_label']]
+    
+    sentiment_counts = {'Positive': sentiments.count('Positive'), 
+                        'Neutral': sentiments.count('Neutral'), 
+                        'Negative': sentiments.count('Negative')}
+    
+    majority_sentiment = max(sentiment_counts, key=sentiment_counts.get)
+    return sentiment_map[majority_sentiment]
+
+# Function to calculate weighted rolling average
+def weighted_rolling_avg(price_df, sent_df, window_size=6, sentiment_effect=0.01):
+    """
+    Calculate rolling average for sentiment data over x number of days before (window size), weighted by recency
+
+    Parameters:
+    - price_df: processed stock data
+    - sent_df: sentiment data
+    - window_size: number of days before current stock price date to aggregate sentiment for
+    - sentiment_effect: impact of the sentiment
+    
+    """
+    sent_df = sent_df.copy()
+    price_df = price_df.copy()
+
+    sent_df['aggregated_sentiment'] = sent_df.apply(aggregate_sentiment, axis=1)
+    sent_df['pub_date'] = to_datetime(sent_df['pub_date'])
+    price_df.index = to_datetime(price_df.index)
+
+    valid_dates = price_df.index
+    weighted_avgs = []
+
+    for current_date in valid_dates:
+        start_date = current_date - timedelta(days=window_size + 1)
+        end_date = current_date - timedelta(days=1)
+        past_data = sent_df[(sent_df['pub_date'] >= start_date) & (sent_df['pub_date'] <= end_date)]
+
+        if not past_data.empty:
+            past_data['weighted'] = (window_size - (current_date - past_data['pub_date']).dt.days + 1) * sentiment_effect * past_data['aggregated_sentiment']
+            weighted_avg = past_data['weighted'].sum()/ past_data.shape[0]
+        else:
+            weighted_avg = 0 # neutral if no sentiment
+        weighted_avgs.append(weighted_avg)
+    
+    price_df['aggregated_sentiment'] = weighted_avgs
+
+    return price_df[['aggregated_sentiment']] # return index of price df + aggregated sentiment in df format
+
+
+if __name__ == '__main__':
+    load_dotenv('../modelling/.weighted.env')
+    target_stock = getenv('target_stock')
+    stock_data_filepath = getenv('stock_data_filepath') + target_stock + '.csv'
+    text_type = getenv('text_type')
+    sentiment_input_filepath = '../../../data/clean/sentiment_analysis_results/finetuned_sentiment_analysis_' + text_type + '_' + target_stock + '.csv'
+    weighted_sentiment_output_filepath = getenv('weighted_sentiment_output_filepath') + text_type + '_' + target_stock + '.csv'
+    
+    lstm_df = read_csv(stock_data_filepath, index_col='Date')
+    sentiment_df = read_csv(sentiment_input_filepath)
+
+    # Aggregate
+    aggregate_sentiment_df = weighted_rolling_avg(lstm_df, sentiment_df, window_size=6, sentiment_effect=0.01) # edit window size and sentiment effect params here
+    aggregate_sentiment_df.to_csv(weighted_sentiment_output_filepath)
